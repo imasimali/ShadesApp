d559209a078b147e82e63a07fe63d35b2a3ad071
Asim Ali <hi@asimali.net>
2024-03-07 16:03:06 +0000

    port shuffled

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const tf = require("@tensorflow/tfjs-node");
 const { trainModel } = require("./trainModel");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
